552dab98292057e008645ac206eb020540d01f1c
Fab <fabo_t@hotmail.com>
2022-07-29 16:17:01 -0400

    purchaseService api updated

--- api/src/main/java/fab/shop/api/core/purchase/PurchaseService.java
+++ api/src/main/java/fab/shop/api/core/purchase/PurchaseService.java
@@ -6,11 +6,42 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import fab.shop.api.composite.PurchaseConfirmRQ;
+import fab.shop.api.core.purchase.msg.*;
 
 
 public interface PurchaseService {
 	
+	@PostMapping(
+		value = "/purchase/purchaseConfirm",
+		consumes = "application/json",
+		produces = "application/json")
+		PurchaseConfirmRS createPurchase(@RequestBody PurchaseConfirmRQ purchaseConfirmRQ);
+
+	@PostMapping(
+			value = "/purchase/purchaseModification",
+			consumes = "application/json",
+			produces = "application/json")
+		  PurchaseModificationRS purchaseModification(@RequestBody PurchaseModificationRQ purchaseModificationRQ);
+
+	@PostMapping(
+			value = "/purchase/purchaseCancel",
+			consumes = "application/json",
+			produces = "application/json")
+		  PurchaseCancelRS purchaseCancel(@RequestBody PurchaseCancelRQ purchaseCancelRQ);
+
+	@PostMapping(
+			value = "/purchase/getPurchase",
+			consumes = "application/json",
+			produces = "application/json")
+		  GetPurchaseRS getPurchase(@RequestBody GetPurchaseRQ getPurchaseRQ);
+
+	@PostMapping(
+			value = "/purchase/getPurchaseList",
+			consumes = "application/json",
+			produces = "application/json")
+		  GetPurchaseListRS getPurchaseList(@RequestBody GetPurchaseListRQ getPurchaseListRQ);
+
+
 	  	@GetMapping(
 			    value = "/purchase/{purchaseId}",
 			    produces = "application/json")
