48caf9d40101c04d98730eada1be7b0fd7a09f3d
fabdeveloper <fabo_t@hotmail.com>
2022-08-25 18:01:52 -0400

    CartMapper

--- microservices/cart-service/src/main/java/fab/shop/microservices/core/cart/persistence/CartRepository.java
+++ microservices/cart-service/src/main/java/fab/shop/microservices/core/cart/persistence/CartRepository.java
@@ -1,7 +1,10 @@
 package fab.shop.microservices.core.cart.persistence;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CartRepository extends CrudRepository<CartEntity, Integer> {
+
+    @Transactional(readOnly = true)
     public CartEntity findByCartId(Integer cartId);
 }
---
+++ microservices/cart-service/src/main/java/fab/shop/microservices/core/cart/services/CartMapper.java
@@ -0,0 +1,24 @@
+package fab.shop.microservices.core.cart.services;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import fab.shop.api.core.cart.Cart;
+import fab.shop.microservices.core.cart.persistence.CartEntity;
+
+
+@Mapper(componentModel = "spring")
+public interface CartMapper {
+
+    @Mappings({
+        @Mapping(target = "serviceAddress", ignore = true)
+    })
+    public Cart entityToApi(CartEntity cartEntity);
+
+    
+    @Mappings({
+        @Mapping(target = "version", ignore = true)
+    })
+    public CartEntity apiToEntity(Cart cart);
+}
