f727adea8565687f83508f39a6814cef9f941b9b
fabdeveloper <fabo_t@hotmail.com>
2022-09-25 14:21:29 -0400

    CartItemMapper - fixed!

--- microservices/cart-service/src/main/java/fab/shop/microservices/core/cart/services/CartMapper.java
+++ microservices/cart-service/src/main/java/fab/shop/microservices/core/cart/services/CartMapper.java
@@ -1,11 +1,14 @@
 package fab.shop.microservices.core.cart.services;
 
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
 
 import fab.shop.api.core.cart.Cart;
 import fab.shop.microservices.core.cart.persistence.CartEntity;
+import fab.shop.microservices.core.cart.persistence.CartItemEntity;
 
 
 @Mapper(componentModel = "spring", uses = { CartItemMapper.class})
@@ -25,4 +28,15 @@ public interface CartMapper {
         @Mapping(target = "modificationDate", ignore = true)
     })
     public CartEntity apiToEntity(Cart cart);
+
+
+    @AfterMapping
+    default void setCartEntity(@MappingTarget CartEntity cartEntity){
+
+        for(CartItemEntity item : cartEntity.getItemsList()){
+            item.setCart(cartEntity);
+        }
+
+
+    }
 }
